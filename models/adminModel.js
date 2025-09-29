423a1f136ae0b075daa308666919b88ce6d8a033
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-09-29 11:35:10 +0600

    admin model

---
+++ models/adminModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const adminSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        maxlength: [30, 'Name cannot exceed 30 characters'],
+        minLength: [4, 'Name must be atleast 4 characters long'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true,
+        validate: [validator.isEmail, 'Please enter a valid email'],
+    },
+    privilege: {
+        type: String,
+        default: 'low'
+    }, 
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minLength: [8, 'Password must be atleast 8 characters long'],
+        select: false
+    }
+})
+
+adminSchema.pre('save', async function (next){
+    if(!this.isModified('password')){
+        return next()
+    }
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+adminSchema.methods.getJwtToken = function () {
+    return jwt.sign({ id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    })
+}
+
+adminSechema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+module.exports = mongoose.model('Admin', adminSchema)
